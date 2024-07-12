e2249e921639e1e4a4d6228c7de34c1a76ec0f94
Omer Can Baykara <omercanbaykara@gmail.com>
2024-07-12 20:45:36 +0300

    Added visuals

--- scripts/background.js
+++ scripts/background.js
@@ -1,24 +1,59 @@
+let urls = []
+
+chrome.storage.onChanged.addListener((changes, area) => {
+    urls = Object.keys(changes.documents.newValue);
+    updateBadge();
+})
+
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
         target: {tabId: tab.id},
         files: ['scripts/content.js'],
     });
-    // chrome.action.setBadgeBackgroundColor({
-    //     'color': 'green',
-    //     'tabId': tab.id
-    // });
-    chrome.action.setBadgeText({
-        'text':"hi",
-        'tabId': tab.id
-    });
-    chrome.action.setIcon({path: "../images/saved.png", tabId: tab.id});
+    updateBadge();
+    console.log(urls);
 });
 
-// chrome.action.onClicked.addListener(function(tab) {
-//     
-    
-//     // Optionally, you can change it back after some time or based on another event
-//     setTimeout(() => {
-//         chrome.action.setIcon({path: "images/unsaved.png", tabId: tab.id});
-//     }, 1000); // Change the icon back after 1 second
-// });
+
+function getTabBadgeAndColor(url) { 
+    if (/^https:\/\/www\.google\.com\/search.*/.test(url)){
+        return ["?", "white"]; 
+    } else if (urls.includes(url)) {
+        return ["✓", "green"]; 
+    } else {
+        return ["-", "gray"];
+    }
+
+}
+
+function updateBadge() {
+    chrome.tabs.query(
+        {currentWindow: true, active : true},
+        function(tabArray) {
+            let activeTab = tabArray[0];
+            let pair = getTabBadgeAndColor(activeTab.url);
+            let text = pair[0];
+            let color = pair[1];
+            chrome.action.setBadgeText(
+                {
+                    text: text,
+                    tabId: activeTab.tabId,
+                });
+            chrome.action.setBadgeBackgroundColor(
+                {
+                    color: color,
+                    tabId: activeTab.tabId,
+                }
+            )
+            console.log(text);
+            console.log(color);
+        }
+    )
+}
+
+chrome.tabs.onActivated.addListener(updateBadge);
+chrome.tabs.onUpdated.addListener(updateBadge);
+// chrome.action.setBadgeBackgroundColor({ color: '#00FF00' });
+// chrome.action.setBadgeText({'text':"hi"});
+// let storage = chrome.storage.local.get(["documents"]);
+// console.log(storage)
